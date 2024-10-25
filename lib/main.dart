82800f7f3262cfb82777ab9aebe086e9f53a844c
shreya <shreyachovatiya171@gmail.com>
2024-10-25 15:25:06 +0530

    initial commit1

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo123',
       home: PaginatedListView(),
     );
   }
